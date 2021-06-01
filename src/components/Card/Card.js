c82d9bef774b3afb603954165f0a67c89414f197
Sean Nguyen <nsean321@gmail.com>
2021-06-01 11:38:58 -0700

    build input

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,34 +1,100 @@
-import React from 'react'
-import { Main, Select, Wrapper, SelectWrapper, Input } from './Card.elements'
-import { options } from '../../utils/Data'
+import { useState, useEffect, useRef } from 'react'
+import { Main, Select, Wrapper, SelectWrapper, Input, SelectContainer, SearchItem, Option, Display, DisplayContainer, AmountContainer } from './Card.elements'
+import { options, name } from '../../utils/Data'
+import Context from '../../utils/Context'
 
 const Card = () => {
 
+  const {
+    input, inputChange,
+    sOptions, setSOptions,
+    setInput, handleClickSearchItem,
+    ref, isVisible, setVisible, transition, setTransition, handleHideDrop, handleShowDrop, handleClickOutside,
+  } = Context()
 
-  const RenderOptions = ({ type }) => {
-    const optionsList = options.map((option, index) =>
-      <option key={index + 1} value={option} > {option}</option >
-    )
-    return (
-      <select>
-        <option key={'start'} value={type == 'start' ? 'USD' : 'EUR'} >{type == 'start' ? 'USD' : 'EUR'}</option>
-        {optionsList}
-      </select>
-    )
-  }
+
+
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  });
 
   return (
     <>
-      {console.log(options)}
       <Main>
-        <Wrapper>
-          <Input></Input>
-          <SelectWrapper>
-            <RenderOptions type={'start'} />
-          </SelectWrapper>
-          <SelectWrapper>
-            <RenderOptions type={'end'} />
-          </SelectWrapper>
+        <Wrapper ref={ref}>
+          <AmountContainer>
+            <Input type={'Number'} placeholder={'AMOUNT'} />
+          </AmountContainer>
+
+
+          <DisplayContainer>
+            <Display id={'fromDisplay'} transition={transition.fromSearch}>
+              <p style={{ fontWeight: 600 }}>{input.fromDisplay.slice(0, 3)}&nbsp;</p>
+              <p>{input.fromDisplay.slice(4)}</p>
+            </Display>
+            <SelectContainer >
+              <Input
+                // onSubmit
+                autocomplete="off"
+                onClick={handleShowDrop}
+                onChange={inputChange}
+                value={input.fromSearch}
+                name={'fromSearch'}
+                placeholder={''}
+                onFocus={(event) => {
+                  setTimeout(() => {
+                    event.target.placeholder = 'FROM'
+                  }, 200);
+                }}
+                onBlur={({ target }) => {
+                  target.placeholder = ''
+                  setInput({ ...input, fromSearch: '' })
+                }}
+              />
+              {isVisible.fromSearch &&
+                <SelectWrapper show={transition.fromSearch}>
+                  <ul>
+                    {sOptions.fromSearch.map((option, index) =>
+                      <SearchItem
+                        key={index + 1}
+                        onClick={() => handleClickSearchItem('from', 'fromDisplay', option)}
+                      >
+                        <Option>{option.key} - {option.name}</Option>
+                      </SearchItem>
+                    )}
+                  </ul>
+                </SelectWrapper>
+              }
+            </SelectContainer>
+          </DisplayContainer>
+
+          {/* <SelectContainer >
+            <Input
+              autocomplete="false"
+              onClick={handleShowDrop}
+              onChange={inputChange}
+              value={input.toSearch}
+              name={'toSearch'}
+              placeholder={'TO'} />
+            {isVisible.toSearch &&
+              <SelectWrapper show={transition.toSearch}>
+                <ul>
+                  {sOptions.toSearch.map((option, index) =>
+                    <li key={index + 1} >
+                      <SearchItem>
+                        <Option>{option.key} - {option.name}</Option>
+                      </SearchItem>
+                    </li >
+                  )}
+                </ul>
+              </SelectWrapper>
+            }
+          </SelectContainer> */}
+
         </Wrapper>
       </Main>
     </>
