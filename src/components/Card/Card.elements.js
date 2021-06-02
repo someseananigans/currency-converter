120c49a22fe96d0e1588a0fee1c84f09d61f6d41
Sean Nguyen <nsean321@gmail.com>
2021-06-02 11:52:11 -0700

    Functionality and styling

--- src/components/Card/Card.elements.js
+++ src/components/Card/Card.elements.js
@@ -1,32 +1,90 @@
 import styled from 'styled-components'
 
 
+export const Label = styled.h4`
+  transition: all .2s ease-in-out;
+  color: ${({ highlight }) => highlight ? '#7ccfff' : '#464646'};
+`
+
 export const Main = styled.div`
-  height: 300px;
-  width: 100%;
+  height: fit-content;
   background: #ffffff;
   display: flex;
-  align-items: center;
+  /* align-items: center; */
+  padding: 30px;
+  justify-content: center;
+  border-radius: 10px;
+  width: 900px;
+  box-shadow: 2px 2px 15px -5px #000000;
 `
+
+export const Result = styled.div`
+  height: 300px;
+  /* background: #ffffff; */
+  display: flex;
+  /* padding: 30px; */
+  justify-content: space-between;
+  /* border-radius: 10px; */
+  width: 900px;
+  /* box-shadow: 2px 2px 15px -5px #000000; */
+  top: 220px;
+  transition: .3s all ease-in-out;
+  opacity: ${({ transition }) => transition ? 1 : 0};
+  transform: ${({ transition }) => transition ? 'translateY(0) ' : 'translateY(50px) '};
+  position: absolute;
+`
+
+export const InputWrapper = styled.div`
+  display: flex; 
+  width: 100%;
+  align-items: center;
+  `
 
 export const Wrapper = styled.div`
   display: flex;
-`
+  flex-direction: column;
+  `
 
-export const Input = styled.input`
-  height: 40px;
-  width: 250px;
+// export const ResultsWrapper = styled.div`
+//   display: flex;
+
+
+//   width: 100%;
+//   `
+
+export const SelectWrapper = styled.div`
+  overflow-y: scroll;
+  max-height: 400px;
+  position: absolute;
+  top: 50px;
+  background: #fafafa;
+  padding: 5px;
   border-radius: 5px;
-  border: 1px #7a7a7a solid;
-  padding: 0 10px;
-  font-weight: 700;
-    &:focus {
-    border: 1px #00ff55 solid;
-      outline: none;
+  transition: .3s all ease-in-out;
+  opacity: ${({ show }) => show ? 1 : 0};
+  transform: ${({ show }) => show ? 'translateY(0) ' : 'translateY(5px) '};
+  z-index: 1;
+  `
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  margin: 15px 0 0 0;
+  padding: 0 7px;
+  `
+
+
+export const SectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  position: relative;
+  margin: 0 6px;
+  &:hover ${Label} {
+    color: #7ccfff;
   }
-  transition: .2s all ease-in-out;
-  background: none !important;
-`
+  `
+
 
 export const SelectContainer = styled.div`
   display: flex;
@@ -36,69 +94,128 @@ export const SelectContainer = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-`
+  `
 
-export const AmountContainer = styled.div`
+export const DisplayWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 250px;
+  height: 45px;
+  padding: 0 15px;
   position: relative;
-  margin: 10px;
-`
-
-export const SelectWrapper = styled.div`
-  overflow-y: scroll;
-  max-height: 400px;
-  position: absolute;
-  top: 50px;
-  background: white;
-  padding: 5px;
-  border-radius: 10px;
-  transition: .3s all ease-in-out;
-  opacity: ${({ show }) => show ? 1 : 0};
-  transform: ${({ show }) => show ? 'translateY(0) ' : 'translateY(5px) '};
-`
+  justify-content: center;
+  `
 
 export const SearchItem = styled.li`
   display: flex;
   height: 50px; 
   width: 100%;
+  cursor: pointer;
   align-items: center;
   &:hover {
     background: gray;
   }
-
-`
-
-export const DisplayContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 250px;
-  position: relative;
-  margin: 10px;
-`
+  
+  `
 
 export const Display = styled.div`
   display: flex;
-  height:100%; 
   width: 100%;
-  border-radius: 5px;
-  border: 1px #7a7a7a solid;
-  padding: 0 10px;
+  /* border-radius: 5px;
+  border: 1px #7a7a7a solid; */
+  
   align-items: center;
+  white-space: nowrap;
   transition: .2s all ease-in-out;
-
   opacity: ${({ transition }) => transition ? 0 : 1};
 `
 
+export const CurrencyDisplay = styled.p`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
 
 
+export const Input = styled.input`
+  height: 45px;
+  width: 250px;
+  border-radius: 5px;
+  border: 1px #c5c5c5 solid;
+  padding: 0 15px;
+  font-weight: 700;
+
+  &:focus {
+    border: 1px #00ff55 solid;
+    outline: none;
+    cursor: text !important;
+  }
+  &:hover {
+    cursor: pointer;
+  }
+  transition: .2s all ease-in-out;
+  background: none !important;
+`
 
 export const Option = styled.p`
-  margin-left: 5px;
+  margin-left: 10px;
   font-weight: 500;
 `
 
 
+export const ConvertButton = styled.button`
+  height: 45px;
+  width: 100%;
+  font-weight: 800;
+  border-radius: 5px;
+  border: none;
+  background: #c7ffb1;
+  color: #424242;
+  align-self: center;
+  cursor: pointer;
+  transition: all .3s ease-in-out;
+  border: 1px solid transparent;
+  &:hover {
+    background: #aeecff;
+    color: #424242;
+    border: 1px solid gray;
+    
+  }
+`
 
+export const Switch = styled.div`
+  width: 40px;
+  border: 1px solid #c5c5c5;
+  border-radius: 50%;
+  padding: 6px;
+  cursor: pointer;
+  color: #57ff57;
+  
+  margin: 21px 7px 0 7px;
+  &:hover {
+    color: white;
+    background: #383838;
+  }
+`
+
+export const RatesContainer = styled.div`
+  width: 49%;
+  display: flex;
+  /* margin: 7px; */
+  flex-direction: column;
+  box-shadow: 2px 2px 15px -5px #000000;
+  border-radius: 10px;
+  background: white;
+`
+
+export const RatesHeader = styled.div`
+  width: 100%;
+  height: 55px;
+  color: #292929;
+  background: #c7ffb1;
+  border-radius: 10px 10px 0 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,25 @@
 import { useState, useEffect, useRef } from 'react'
-import { Main, Select, Wrapper, SelectWrapper, Input, SelectContainer, SearchItem, Option, Display, DisplayContainer, AmountContainer } from './Card.elements'
+import { Main, Select, InputWrapper, SelectWrapper, Wrapper, ResultsWrapper, Input, SelectContainer, SearchItem, Option, Display, DisplayWrapper, SectionContainer, ConvertButton, ButtonWrapper, Switch, CurrencyDisplay, RatesContainer, RatesHeader, Result, Label } from './Card.elements'
 import { options, name } from '../../utils/Data'
 import Context from '../../utils/Context'
+import styled from 'styled-components'
+import { ArrowLeftRight } from '@styled-icons/bootstrap/'
+import { ArrowSwap } from '@styled-icons/fluentui-system-filled/'
 
 const Card = () => {
 
   const {
-    input, inputChange,
+    input, inputChange, rates,
     sOptions, setSOptions,
     setInput, handleClickSearchItem,
-    ref, isVisible, setVisible, transition, setTransition, handleHideDrop, handleShowDrop, handleClickOutside,
+    convert, getExchange, exchange,
+    ref, isVisible, setVisible, transition, setTransition, handleHideDrop, handleShowDrop, handleClickOutside, results
   } = Context()
 
 
-
+  useEffect(() => {
+    getExchange()
+  }, [])
 
   useEffect(() => {
     document.addEventListener('click', handleClickOutside, true);
@@ -22,81 +28,173 @@ const Card = () => {
     };
   });
 
+  function numberWithCommas(n) {
+    var parts = n.toString().split(".");
+    return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : "");
+  }
+
   return (
     <>
-      <Main>
-        <Wrapper ref={ref}>
-          <AmountContainer>
-            <Input type={'Number'} placeholder={'AMOUNT'} />
-          </AmountContainer>
+      <div style={{ position: 'relative', alignItems: 'center', display: 'flex', flexDirection: 'column', height: '555px' }}>
+        <Main>
+          <Wrapper>
+
+            <InputWrapper ref={ref}>
+              <SectionContainer>
+                <Label highlight={input.amount}>Amount</Label>
+                <form onSubmit={convert}>
+                  <Input
+                    type={'Number'}
+                    name={'amount'}
+                    placeholder={'AMOUNT'}
+                    value={input.amount}
+                    onChange={inputChange}
+                    onClick={handleHideDrop}
+                  />
+                </form>
+              </SectionContainer>
+
+              <SectionContainer>
+
+                <Label highlight={transition.fromSearch}>From</Label>
+                <DisplayWrapper>
+                  <Display id={'fromDisplay'} transition={transition.fromSearch}>
+                    <p style={{ fontWeight: 600 }}>
+                      {input.fromDisplay.slice(0, 3)}&nbsp;
+                  </p>
+                    <CurrencyDisplay>
+                      {input.fromDisplay.slice(4)}
+                    </CurrencyDisplay>
+                  </Display>
+                  <SelectContainer >
+                    <Input
+                      // onSubmit
+                      autocomplete="off"
+                      onClick={handleShowDrop}
+                      onChange={inputChange}
+                      value={input.fromSearch}
+                      name={'fromSearch'}
+                      placeholder={''}
+                      onFocus={(event) => {
+                        setTimeout(() => {
+                          event.target.placeholder = 'FROM'
+                        }, 200);
+                      }}
+                      onBlur={({ target }) => {
+                        target.placeholder = ''
+                        setInput({ ...input, fromSearch: '' })
+                      }}
+                    />
+                    {isVisible.fromSearch &&
+                      <SelectWrapper show={transition.fromSearch}>
+                        <ul>
+                          {sOptions.fromSearch.map((option, index) =>
+                            <SearchItem
+                              key={index + 1}
+                              onClick={() => handleClickSearchItem('from', 'fromDisplay', option)}
+                            >
+                              <Option>{option.key} - {option.name}</Option>
+                            </SearchItem>
+                          )}
+                        </ul>
+                      </SelectWrapper>
+                    }
+                  </SelectContainer>
+                </DisplayWrapper>
+              </SectionContainer>
+
+              <Switch>
+                <ArrowSwap />
+              </Switch>
+
+              <SectionContainer>
+                <Label highlight={transition.toSearch}>To</Label>
+                <DisplayWrapper>
+                  <Display id={'toDisplay'} transition={transition.toSearch}>
+                    <p style={{ fontWeight: 600 }}>
+                      {input.toDisplay.slice(0, 3)}&nbsp;
+                  </p>
+                    <CurrencyDisplay>
+                      {input.toDisplay.slice(4)}
+                    </CurrencyDisplay>
+                  </Display>
+                  <SelectContainer >
+                    <Input
+                      // onSubmit
+                      autocomplete="off"
+                      onClick={handleShowDrop}
+                      onChange={inputChange}
+                      value={input.toSearch}
+                      name={'toSearch'}
+                      placeholder={''}
+                      onFocus={(event) => {
+                        setTimeout(() => {
+                          event.target.placeholder = 'TO'
+                        }, 200);
+                      }}
+                      onBlur={({ target }) => {
+                        target.placeholder = ''
+                        setInput({ ...input, toSearch: '' })
+                      }}
+                    />
+                    {isVisible.toSearch &&
+                      <SelectWrapper show={transition.toSearch}>
+                        <ul>
+                          {sOptions.toSearch.map((option, index) =>
+                            <SearchItem
+                              key={index + 1}
+                              onClick={() => handleClickSearchItem('to', 'toDisplay', option)}
+                            >
+                              <Option>{option.key} - {option.name}</Option>
+                            </SearchItem>
+                          )}
+                        </ul>
+                      </SelectWrapper>
+                    }
+                  </SelectContainer>
+                </DisplayWrapper>
+              </SectionContainer>
+            </InputWrapper>
+
+            <ButtonWrapper>
+              <ConvertButton onClick={convert}>CONVERT</ConvertButton>
+
+            </ButtonWrapper>
 
 
-          <DisplayContainer>
-            <Display id={'fromDisplay'} transition={transition.fromSearch}>
-              <p style={{ fontWeight: 600 }}>{input.fromDisplay.slice(0, 3)}&nbsp;</p>
-              <p>{input.fromDisplay.slice(4)}</p>
-            </Display>
-            <SelectContainer >
-              <Input
-                // onSubmit
-                autocomplete="off"
-                onClick={handleShowDrop}
-                onChange={inputChange}
-                value={input.fromSearch}
-                name={'fromSearch'}
-                placeholder={''}
-                onFocus={(event) => {
-                  setTimeout(() => {
-                    event.target.placeholder = 'FROM'
-                  }, 200);
-                }}
-                onBlur={({ target }) => {
-                  target.placeholder = ''
-                  setInput({ ...input, fromSearch: '' })
-                }}
-              />
-              {isVisible.fromSearch &&
-                <SelectWrapper show={transition.fromSearch}>
-                  <ul>
-                    {sOptions.fromSearch.map((option, index) =>
-                      <SearchItem
-                        key={index + 1}
-                        onClick={() => handleClickSearchItem('from', 'fromDisplay', option)}
-                      >
-                        <Option>{option.key} - {option.name}</Option>
-                      </SearchItem>
-                    )}
-                  </ul>
-                </SelectWrapper>
-              }
-            </SelectContainer>
-          </DisplayContainer>
 
-          {/* <SelectContainer >
-            <Input
-              autocomplete="false"
-              onClick={handleShowDrop}
-              onChange={inputChange}
-              value={input.toSearch}
-              name={'toSearch'}
-              placeholder={'TO'} />
-            {isVisible.toSearch &&
-              <SelectWrapper show={transition.toSearch}>
-                <ul>
-                  {sOptions.toSearch.map((option, index) =>
-                    <li key={index + 1} >
-                      <SearchItem>
-                        <Option>{option.key} - {option.name}</Option>
-                      </SearchItem>
-                    </li >
-                  )}
-                </ul>
-              </SelectWrapper>
-            }
-          </SelectContainer> */}
+          </Wrapper>
 
-        </Wrapper>
-      </Main>
+        </Main>
+
+        {results.reveal &&
+          <Result transition={results.transition}>
+
+
+
+
+            <RatesContainer>
+              <RatesHeader>
+                <strong>RATE</strong>
+              </RatesHeader>
+              <p>1 {input.from} = </p>
+              <p>{exchange.xRate.toFixed(5)} {input.to} </p>
+              <p>1 {input.to} = </p>
+              <p>{(1 / exchange.xRate).toFixed(5)} {input.from} </p>
+            </RatesContainer>
+            <RatesContainer>
+              <RatesHeader>
+                <strong>CONVERSION TOTAL</strong>
+              </RatesHeader>
+              <p>{numberWithCommas(parseFloat(input.amount))} {input.from} = </p>
+              <p>{numberWithCommas(parseFloat(exchange.total).toFixed(5))} {input.to}</p>
+            </RatesContainer>
+
+
+
+          </Result>
+        }
+      </div>
     </>
   )
 }
