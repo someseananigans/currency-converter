f4577365fa32776abf5a1573ff1207b28077c68f
Sean Nguyen <nsean321@gmail.com>
2021-06-02 11:52:23 -0700

    formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,41 +2,47 @@ import { useEffect } from 'react'
 import Context from '../utils/Context'
 import styled from 'styled-components'
 import Card from '../components/Card/Card'
+import exchange from '../assets/exchange.svg'
+import background from '../assets/background.jpg'
+import background2 from '../assets/background2.jpg'
+import background3 from '../assets/background3.jpg'
 
 const Home = () => {
 
-  const {
-    getExchange,
-    inputChange,
-    convert,
-    rates, setRates,
-    input, setInput,
-    exchange, setExchange,
-    isLoading, setLoading
-  } = Context()
-
-  useEffect(() => {
-    // getExchange()
-    // setTimeout(() => {
-
-    //   console.log(rates)
-    // }, 1000);
-  }, [input])
-
-
   return (
     <>
-      <Container>
+      <Main>
+        <Header>Currency Exchange</Header>
         <Card />
-      </Container>
+      </Main>
     </>
   )
 }
 
 export default Home
 
-const Container = styled.div`
+const Main = styled.div`
+  padding: 100px;
   min-height: 100vh;
-  height: 100vh;
-  background: #a9ffa9;
+  height: 100%;
+  background: #333333;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-image: url(${background3});
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+`
+const Image = styled.img`
+  width: 500px;
+  margin: 20px;
+`
+
+const Header = styled.h1`
+  font-size: 40px;
+  color: white;
+  text-shadow: 2px 2px 5px black;
+  padding-bottom: 20px;
 `
